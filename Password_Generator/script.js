dfd3b5e1ce98fa19849f592115da3c3ae2c13d19
vishal <vishalcode247@gmail.com>
2025-07-06 18:09:04 +0530

    add script file

--- Password_Generator/script.js
+++ Password_Generator/script.js
@@ -6,6 +6,7 @@ let Uppercase  = document.getElementById('Uppercase');
 let Numbers = document.getElementById('Numbers');
 let Symbols = document.getElementById('Symbols');
 let genBtn = document.getElementById('genBtn');
+let copyIcon = document.getElementById('copyIcon');
 
 //used for show slider value
 slidervalue.textContent = inputslider.value;
@@ -21,13 +22,35 @@ genBtn.addEventListener('click', ()=>{
 let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lower = "abcdefghijklmnopqrstuvwxyz"
 let num = "0123456789"
-let Symbol = "@_."
+let symbol = "@_."
 
 
 //function for generate password
 function generatePassword(){
     let genPassword = "";
+    let allChar = "";
 
-    genPassword = upper.charAt(Math.floor(Math.random()*upper.length));
+    allChar += Uppercase.checked ? upper : "";
+    allChar += lowercase.checked ? lower : "";
+    allChar += Numbers.checked ? num : "";
+    allChar += Symbols.checked ? symbol : "";
+
+    if(allChar == "" || allChar.length == 0){
+        return genPassword;
+    }
+
+    let i = 1;
+    while(i<=inputslider.value){
+    genPassword += allChar.charAt(Math.floor(Math.random() * allChar.length));
+    i++;
+    }
     return genPassword;
 }
+
+copyIcon.addEventListener('click', ()=>{
+    if(passBox.value != ""){
+    navigator.clipboard.writeText(passBox.value);
+    // copyIcon.innerHTML = "check";
+    copyIcon.title = "copied";
+    }
+});
