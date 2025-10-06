f87304b2dd327ff5049d4228746c4914a8f94a65
vishal <vishalcode247@gmail.com>
2025-10-06 16:48:06 +0530

    project done

--- AutoTextGen/script.js
+++ AutoTextGen/script.js
@@ -5,33 +5,38 @@ const cursor = document.querySelector('.cursor');
 // console.log(cursor);/
 
 let words = ["Awesome", "Fun", "Cool", "Life", "Famous"];
-const typingDelay = 200;
-const erasingDelay = 200;
+const typingDelay = 60;
+const erasingDelay = 50;
 const newLetterDelay = 2000;
 let index = 0;
 let charIndex = 0;
 
+document.addEventListener("DOMContentLoaded", function () {
+    if (words.length) setTimeout(type, newLetterDelay + 250);
+});
+
 function type() {
-    if(charIndex < words[index.length]){
+    if (charIndex < words[index].length) {
         typedText.textContent += words[index].charAt(charIndex);
         charIndex++;
         setTimeout(type, typingDelay);
     }
-    else{
-        settimeout(erase, newLetterDelay);
+    else {
+        setTimeout(erase, newLetterDelay);
     }
 }
 
 function erase() {
-    if(charIndex > 0){
+    if (charIndex > 0) {
         typedText.textContent = words[index].substring(0, charIndex - 1);
         charIndex--;
-        setTimeout(erase, erasingDelay);            
+        setTimeout(erase, erasingDelay);
     }
-    else{
+    else {
         index++;
-        if(index >= words.length) 
-        index = 0;
+        if (index >= words.length)
+            index = 0;
+        setTimeout(type, typingDelay + 1100);
+
     }
-    setTimeout(type, typingDelay + 1100);
 }
