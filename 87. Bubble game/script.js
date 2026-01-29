400a9dadf651d095e030ccee57f1184c371b7296
vishal <vishalcode247@gmail.com>
2026-01-29 15:19:35 +0530

    project 87 complete

---
+++ 87. Bubble game/script.js
@@ -0,0 +1,51 @@
+let hitrn = 0;
+
+function makeBubble() {
+    let clutter = "";
+
+    for (let i = 0; i <= 206; i++) {
+        let rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+makeBubble();
+
+let timer = 60;
+function runTimer() {
+    let timerint = setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timer").textContent = timer;
+        } else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = "<h1>Game Over!!</h1>";
+        }
+    }, 1000);
+}
+
+runTimer();
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+getNewHit();
+
+let score = 0;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+document.querySelector("#pbtm").addEventListener("click", (dets) => {
+    let clickednum = Number(dets.target.textContent);
+    if(clickednum == hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
