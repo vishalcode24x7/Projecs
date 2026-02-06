f1eb2bd5d6c6dd35a8a92142e81b195579f5751c
vishal <vishalcode247@gmail.com>
2026-02-06 20:22:52 +0530

    project 95 complete

---
+++ 95. duostudio clone/script.js
@@ -0,0 +1,69 @@
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+init();
+
+let cursor = document.querySelector('.cursor')
+let main = document.querySelector(".main")
+main.addEventListener("mousemove", (e)=>{
+    cursor.style.left = e.x+"px";
+    cursor.style.top = e.y+"px";
+})
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        markers: true,
+        start: "top 27%",
+        end: "top 0",
+        scrub: 2
+    }
+})
+tl.to(".page1 h1", {
+    x: -100
+},"anim")
+
+tl.to(".page1 h2",{
+    x:100
+},"anim")
+
+tl.to(".page1 video",{
+    width: "90%"
+},"anim")
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        markers: true,
+        start: "top -110%",
+        end: "top -120%",
+        scrub: 3
+    }
+})
+
+tl2.to(".main",{
+    backgroundColor:"#fff"
+})
